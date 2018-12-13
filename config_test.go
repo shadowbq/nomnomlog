3c811c8d5a9b4ac10bceb78d434bda44a7115c05
shadowbq <shadowbq@gmail.com>
2018-12-13 12:27:41 -0500

    can introspect/reflect original regexmatch string in test

--- config_test.go
+++ config_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"regexp"
 	"testing"
 	"time"
@@ -13,6 +14,7 @@ import (
 func TestRawConfig(t *testing.T) {
 	assert := assert.New(t)
 	initConfigAndFlags()
+	log.Infof("initConfigAndFlags()")
 
 	// pretend like some things were passed on the command line
 	flags.Set("configfile", "test/config.yaml")
@@ -26,7 +28,17 @@ func TestRawConfig(t *testing.T) {
 	assert.Equal(c.Destination.Host, "logs.papertrailapp.com")
 	assert.Equal(c.Destination.Port, 514)
 	assert.Equal(c.Destination.Protocol, "tls")
-	// assert.Equal(c.IncludePatterns, []*regexp.Regexp{regexp.MustCompile("log only me"), regexp.MustCompile(`log o.{1,2} me`)})
+	//c.IncludePatterns
+
+	assert.Equal("string", reflect.TypeOf("log only bme").String(), "reflected types match")
+	assert.Equal("[]*regexp.Regexp", reflect.TypeOf(c.IncludePatterns).String(), "reflected IncludePatterns types match")
+
+	assert.Equal(c.IncludePatterns[0].String(), "log only me", "IncludePattern #0 regString matches config string")
+	assert.Equal(c.IncludePatterns[1].String(), `log o.{1,2} me`, "IncludePattern #1 regString matches config string")
+
+	//fmt.Println(reflect.TypeOf("strup"))
+
+	assert.Equal(c.IncludePatterns, []*regexp.Regexp{regexp.MustCompile("log only me"), regexp.MustCompile(`log o.{1,2} me`)}, "Include Patterns should be regexp")
 	assert.Equal(c.ExcludePatterns, []*regexp.Regexp{regexp.MustCompile("don't log on me"), regexp.MustCompile(`do \w+ on me`)})
 	assert.Equal(c.ExcludeFiles, []*regexp.Regexp{regexp.MustCompile(`\.DS_Store`)})
 	assert.Equal(c.Files, []LogFile{
